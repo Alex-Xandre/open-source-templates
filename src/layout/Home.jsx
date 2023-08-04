e33f40d031d0ab87d4ee6c411d66e4fa7c90de25
Xandre Micua <xndrmcua22@gmail.com>
2023-08-05 01:24:20 +0800

    updated links

--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -63,7 +63,7 @@ const Home = () => {
         <div className="flex align-center justify-center md:block">
           <button
             className={`bg-slate-700 font-[500] hover:bg-slate-800 py-2 px-8 rounded text-white m-0 flex align-center justify-center text-sm lg:ml-2 `}
-            onClick={() => navigate("/about")}
+            onClick={() => navigate("/projects")}
           >
             View All{" "}
             <span className=" ">
